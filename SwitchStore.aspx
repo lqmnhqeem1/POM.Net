615cc2d08a0a383768b21b9eaf6aeca7076d9d08
siliangnPPC <siliangn@purpleconnect.com>
2023-04-10 09:44:17 +0800

    Add Switch Store

---
+++ SwitchStore.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="SwitchStore.aspx.vb" Inherits="IM_POM_VB.Net.SwitchStore" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Switch Store</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/pom.net.css" rel="stylesheet" type="text/css" />
+     <link href="/css/styles.css?version=1" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript" src="commonjs/common.js"></script>
+
+    <script type="text/javascript" src="commonjs/rootlevelpage.js"></script>
+
+    <script type="text/javascript" language="javascript">
+            function checkKey(event)
+            {
+                var defcontrol;
+                
+                switch(event.keyCode)
+                {
+                    case 13:
+                        defcontrol = document.getElementById('btnOk');
+                        if(defcontrol) defcontrol.focus();
+                        
+                        break;
+                    
+                    case 27:
+                        defcontrol = document.getElementById('btnCancel');
+                        if(defcontrol) defcontrol.click();
+                        
+                        break;
+                }
+            }
+    </script>
+    <style type="text/css">
+        div.main {
+    background: #5fb0e4;
+    background: -moz-radial-gradient(center, ellipse cover, #0264d6 1%, #1c2b5a 100%);
+    background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(1%,#0264d6), color-stop(100%,#1c2b5a));
+    background: -webkit-radial-gradient(center, ellipse cover, #0264d6 1%,#1c2b5a 100%);
+    background: -o-radial-gradient(center, ellipse cover, #0264d6 1%,#1c2b5a 100%);
+    background: -ms-radial-gradient(center, ellipse cover, #0264d6 1%,#1c2b5a 100%);
+    background: radial-gradient(ellipse at center, #7db97e 1%,#1c2b5a 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0264d6', endColorstr='#1c2b5a',GradientType=1 );
+    height: calc(100vh);
+    width: 100%;
+}
+    </style>
+
+</head>
+<body onclick="checkKey(event);">
+    <div class="main">
+         <form id="frmSwitchStore" runat="server" method="post">
+        <center>
+            <h1  class="mt-5 font-weight-bold " style="color: #eee;">POM.NET</h1>
+                
+                <asp:Image ID="Image1" runat="server" ImageUrl="../images/Giant_logo.png" AlternateText="Giant" />
+
+            <div class="container bg-light mt-3"  style="border-style:solid; width:35%;">
+                    <div>
+                      
+                            <asp:Label ID="lblUser" runat="server" CssClass="text-dark " Text="User: "></asp:Label>
+                             <asp:Label ID="lblUserId" runat="server" CssClass="text-dark" ForeColor="black"
+                                                            Font-Bold="true" Text=""></asp:Label>
+                            <a id="lnkSignOut" runat="server">
+                                <asp:Label ID="lblSignOut" runat="server" CssClass="btn btn-outline-danger ml-2" Font-Bold="true"
+                                                                Text="Sign Out"></asp:Label>
+                             </a>
+                       
+                        </div>
+                   
+                                           <hr />
+                         <asp:Label ID="lblPageTile" runat="server" CssClass="h3 text-dark font-weight-bold d-flex justify-content-center" Text="Switch Store"></asp:Label>
+                        <asp:Label ID="lblRoles" runat="server" CssClass="text-dark font-weight-bold d-flex justify-content-start ml-2" 
+                            Text="Select a Store:"></asp:Label>
+                        
+                <div class="mt-3 mb-3">
+                  
+        
+                        <asp:DropDownList ID="ddlStore" runat="server" CssClass="form-control">
+                                </asp:DropDownList>
+                 
+                </div>
+
+                 <div class="mb-3">
+                        <asp:Button ID="btnOk" runat="server" CssClass="btn btn-primary" Text="Ok" />
+                        <asp:Button ID="btnCancel" runat="server" CssClass="btn btn-danger" Text="Cancel" />
+                 </div>
+                        
+
+ 
+
+                </div>
+        </center>
+             <asp:Label ID="lblMessage" runat="server" CssClass="redLabel" Text="" Visible="false"></asp:Label>
+                                     <asp:Label ID="lblStore" runat="server" CssClass="text-dark font-weight-bold mt-2" Text="Store:" Visible="false"></asp:Label>
+
+             </form>
+    </div>
+
+</body>
+</html>
---
+++ SwitchStore.aspx.vb
@@ -0,0 +1,169 @@
+﻿Imports POM.Lib.Data
+Imports POM.Lib.UI
+Imports POM.Lib.Log
+
+Partial Class SwitchStore
+
+    Inherits System.Web.UI.Page
+    Protected screenpx As String = System.Configuration.ConfigurationManager.AppSettings.Item("Screenpx")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+            Context.Response.AddHeader("X-UA-Compatible", "IE=7")
+
+            If Utility.UserId.Equals(String.Empty) Then Response.Redirect("~/Login.aspx")
+
+            'Populate data
+            If Not IsPostBack Then
+                lblUserId.Text = Utility.UserName
+
+                'Jerry Commented: Logo Section Temperory Comment
+                'lblLogoVer.Text = System.Configuration.ConfigurationManager.AppSettings.Item("AppVer")
+
+                'If Utility.UserStoreName.ToString().ToLower().Contains("tmc") Then
+                '    imgGiant.ImageUrl = "~/images/TMCLogo.jpg"
+                'ElseIf Utility.UserStoreName.ToString().ToLower().Contains("mercato") Then
+                '    imgGiant.ImageUrl = "~/images/MercatoLogo.jpg"
+                'ElseIf Utility.UserStoreName.ToString().ToLower().Contains("cold storage") Then
+                '    imgGiant.ImageUrl = "~/images/ColdStorageLogo2.jpg"
+                'ElseIf Utility.UserStoreName.ToString().ToLower().Contains("giant mini") Then
+                '    imgGiant.ImageUrl = "~/images/GiantMini.jpg"
+                'Else
+                '    imgGiant.ImageUrl = "~/images/Giant2.jpg"
+                'End If
+
+                Utility.DropDownDataBind(ddlStore, Constants.Store)
+                Utility.SetDropDownSelectedValue(ddlStore, Utility.UserStoreId)
+                ddlStore.Attributes.Add("onkeypress", "return keySort(this);")
+
+            End If
+
+            'Configure Controls
+            btnOk.OnClientClick = "return validateSwitchStoreEntry('" & ddlStore.ClientID & "', '" & Utility.GetMessage("10005", lblStore.Text) & "');"
+
+        Catch tex As Threading.ThreadAbortException
+            'Ignore
+        Catch ex As Exception
+            Dim objEx As New ApplicationException("Error Loading Page", ex)
+
+            objEx.Source = IIf(IsNothing(ex.InnerException), Reflection.Assembly.GetExecutingAssembly.GetName(False).Name, ex.Source)
+            ExceptionLog.Log(objEx)
+
+            Try
+                Response.Redirect("~/ErrorPage.aspx?code=50001")
+            Catch tex As Threading.ThreadAbortException
+                'Ignore
+            End Try
+
+        End Try
+
+    End Sub
+
+    Protected Sub lnkSignOut_ServerClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles lnkSignOut.ServerClick
+
+        Try
+            Session.Clear()
+            Response.Redirect("~/Login.aspx")
+
+        Catch tex As Threading.ThreadAbortException
+            'Ignore
+        Catch ex As Exception
+            Dim objEx As New ApplicationException("Error Signing Out", ex)
+
+            objEx.Source = IIf(IsNothing(ex.InnerException), Reflection.Assembly.GetExecutingAssembly.GetName(False).Name, ex.Source)
+            ExceptionLog.Log(objEx)
+
+            Try
+                Response.Redirect("~/ErrorPage.aspx?code=50001")
+            Catch iex As Threading.ThreadAbortException
+                'Ignore
+            End Try
+
+        End Try
+
+    End Sub
+
+    Protected Sub btnOk_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnOk.Click
+
+        Dim objDataAccess As DataAccess,
+            objDataSet As DataSet
+
+        Try
+            lblMessage.Text = ""
+            lblMessage.Visible = False
+
+            objDataAccess = New DataAccess()
+            objDataSet = objDataAccess.ExecuteSpDataSet("usp_UserAuthentication_Search", DataAccess.BuildXmlParam("UserID", Utility.UserId, "StoreID", ddlStore.SelectedItem.Value))
+
+            If objDataSet.Tables.Count < 2 Then
+                Response.Redirect("~/ErrorPage.aspx?code=50001")
+            Else
+                If CType(objDataSet.Tables(1).Rows(0)("islocal"), Integer) = 1 Then
+                    If CType(objDataSet.Tables(0).Rows(0)("HasAccess"), Integer) = 1 Then
+                        Utility.UserStoreId = ddlStore.SelectedItem.Value
+                        Utility.UserStoreName = ddlStore.SelectedItem.Text
+                        Response.Redirect("~/Home.aspx")
+                    Else
+                        lblMessage.Text = Utility.GetMessage("10004")
+                        lblMessage.Visible = True
+                    End If
+                Else
+                    If CType(objDataSet.Tables(1).Rows(0)("storeip"), String).Trim().Length = 0 Then
+                        lblMessage.Text = Utility.GetMessage("10022")
+                        lblMessage.Visible = True
+                    Else
+                        Response.Redirect("http://" & CType(objDataSet.Tables(1).Rows(0)("storeip"), String) & "/" & ConfigurationManager.AppSettings("PUBLISH_WEB_SITE_NAME") & "/Login.aspx")
+                    End If
+                End If
+            End If
+
+        Catch tex As Threading.ThreadAbortException
+            'Ignore
+        Catch ex As Exception
+            Dim objEx As New ApplicationException("Error Switching Store", ex)
+
+            objEx.Source = IIf(IsNothing(ex.InnerException), Reflection.Assembly.GetExecutingAssembly.GetName(False).Name, ex.Source)
+            ExceptionLog.Log(objEx)
+
+            Try
+                Response.Redirect("~/ErrorPage.aspx?code=50001")
+            Catch iex As Threading.ThreadAbortException
+                'Ignore
+            End Try
+
+        Finally
+            If Not IsNothing(objDataSet) Then objDataSet.Dispose()
+            If Not IsNothing(objDataAccess) Then objDataAccess = Nothing
+        End Try
+
+    End Sub
+
+    Protected Sub btnCancel_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+
+        Try
+            If IsNothing(Request.QueryString("origin")) AndAlso Request.QueryString("origin").Trim().Length > 0 Then
+                Response.Redirect(Request.QueryString("origin"))
+            Else
+                Response.Redirect("~/Home.aspx")
+            End If
+
+        Catch tex As Threading.ThreadAbortException
+            'Ignore
+        Catch ex As Exception
+            Dim objEx As New ApplicationException("Error cancelling store switch", ex)
+
+            objEx.Source = IIf(IsNothing(ex.InnerException), Reflection.Assembly.GetExecutingAssembly.GetName(False).Name, ex.Source)
+
+            ExceptionLog.Log(objEx)
+
+            Try
+                Response.Redirect("~/ErrorPage.aspx?code=50001")
+            Catch iex As Threading.ThreadAbortException
+                'Ignore
+            End Try
+
+        End Try
+
+    End Sub
+End Class
